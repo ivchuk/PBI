255908d9b88fad165ff046783d89d56b55a629a1
Julia Shypat <juliashipat@gmail.com>
2017-12-03 16:35:04 +0100

    first commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var barchart404DC04A8A0C4EEAB692FE0E952925B3_DEBUG = {
+        name: 'barchart404DC04A8A0C4EEAB692FE0E952925B3_DEBUG',
+        displayName: 'barchart',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.9.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.barchart404DC04A8A0C4EEAB692FE0E952925B3.Visual(options),
+        custom: true
+    };
+}
